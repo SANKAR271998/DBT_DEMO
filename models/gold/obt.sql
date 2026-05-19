c26fd4367ed19799f1efd94fa326f8cff3911b0f
Sankar B <sankarrock78@gmail.com>
2026-05-19 16:18:21 +0000

    may 19

---
+++ models/gold/obt.sql
@@ -0,0 +1,30 @@
+{% set congigs = [  
+    {
+        "table" : "SANKAR_DB.silver.silver_EMPLOYEE",
+        "columns" : "silver_employee.employee_id,silver_employee.name,silver_employee.department,silver_employee.salary_category",
+        "alias" : "silver_employee"
+    },
+    {
+        "table" : "SANKAR_DB.silver.silver_EMP",
+        "columns" : "silver_emp.annual_salary,silver_emp.record_created_at,silver_emp.new_salary",
+        "alias" : "silver_emp",
+        "join_condition" : "silver_employee.employee_id = silver_emp.employee_id"
+    }
+]%}
+
+
+SELECT 
+    {% for config in congigs %}
+         {{ config.columns }}{% if not loop.last %},{% endif %}
+    {% endfor %}
+FROM
+    {% for config in congigs %}
+    {% if loop.first %}
+        {{ config['table'] }} AS {{ config ['alias'] }}
+    {% else %}
+    LEFT JOIN {{ config['table'] }} AS {{ config ['alias'] }}
+    ON {{ config['join_condition'] }}
+        {% endif %}
+        {% endfor %}
+
+
--- models/silver/silver_emp.sql
+++ models/silver/silver_emp.sql
@@ -6,6 +6,7 @@
 SELECT
     employee_id,
     first_name,
+    last_name,
     annual_salary,
     {{ multiply('annual_salary', '1.10', 2) }} + 1000 AS new_salary,
     record_created_at
